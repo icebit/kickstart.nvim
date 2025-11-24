6784249583389fcb4e39e019bb435e999b781a1c
icebit <samjharrison8@gmail.com>
2025-11-23 19:00:07 -0600

    init

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -246,6 +246,359 @@ rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+
+    config = function()
+      require('oil').setup {
+        -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+        -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+        default_file_explorer = true,
+        -- Id is automatically added at the beginning, and name at the end
+        -- See :help oil-columns
+        columns = {
+          'icon',
+          -- "permissions",
+          -- "size",
+          -- "mtime",
+        },
+        -- Buffer-local options to use for oil buffers
+        buf_options = {
+          buflisted = false,
+          bufhidden = 'hide',
+        },
+        -- Window-local options to use for oil buffers
+        win_options = {
+          wrap = false,
+          signcolumn = 'no',
+          cursorcolumn = false,
+          foldcolumn = '0',
+          spell = false,
+          list = false,
+          conceallevel = 3,
+          concealcursor = 'nvic',
+        },
+        -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+        delete_to_trash = true,
+        -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+        skip_confirm_for_simple_edits = true,
+        -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+        -- (:help prompt_save_on_select_new_entry)
+        prompt_save_on_select_new_entry = true,
+        -- Oil will automatically delete hidden buffers after this delay
+        -- You can set the delay to false to disable cleanup entirely
+        -- Note that the cleanup process only starts when none of the oil buffers are currently displayed
+        cleanup_delay_ms = 2000,
+        lsp_file_methods = {
+          -- Enable or disable LSP file operations
+          enabled = true,
+          -- Time to wait for LSP file operations to complete before skipping
+          timeout_ms = 1000,
+          -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+          -- Set to "unmodified" to only save unmodified buffers
+          autosave_changes = false,
+        },
+        -- Constrain the cursor to the editable parts of the oil buffer
+        -- Set to `false` to disable, or "name" to keep it on the file names
+        constrain_cursor = 'editable',
+        -- Set to true to watch the filesystem for changes and reload oil
+        watch_for_changes = true,
+        -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+        -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+        -- Additionally, if it is a string that matches "actions.<name>",
+        -- it will use the mapping at require("oil.actions").<name>
+        -- Set to `false` to remove a keymap
+        -- See :help oil-actions for a list of all available actions
+        keymaps = {
+          ['g?'] = { 'actions.show_help', mode = 'n' },
+          ['<CR>'] = 'actions.select',
+          ['<C-s>'] = { 'actions.select', opts = { vertical = true } },
+          ['<C-h>'] = { 'actions.select', opts = { horizontal = true } },
+          ['<C-t>'] = { 'actions.select', opts = { tab = true } },
+          ['<C-p>'] = 'actions.preview',
+          ['<C-c>'] = { 'actions.close', mode = 'n' },
+          ['<C-l>'] = 'actions.refresh',
+          ['-'] = { 'actions.parent', mode = 'n' },
+          ['_'] = { 'actions.open_cwd', mode = 'n' },
+          ['`'] = { 'actions.cd', mode = 'n' },
+          ['~'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
+          ['gs'] = { 'actions.change_sort', mode = 'n' },
+          ['gx'] = 'actions.open_external',
+          ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
+          ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
+        },
+        -- Set to false to disable all of the above keymaps
+        use_default_keymaps = true,
+        view_options = {
+          -- Show files and directories that start with "."
+          show_hidden = false,
+          -- This function defines what is considered a "hidden" file
+          is_hidden_file = function(name, bufnr)
+            local m = name:match '^%.'
+            return m ~= nil
+          end,
+          -- This function defines what will never be shown, even when `show_hidden` is set
+          is_always_hidden = function(name, bufnr)
+            return false
+          end,
+          -- Sort file names with numbers in a more intuitive order for humans.
+          -- Can be "fast", true, or false. "fast" will turn it off for large directories.
+          natural_order = 'fast',
+          -- Sort file and directory names case insensitive
+          case_insensitive = false,
+          sort = {
+            -- sort order can be "asc" or "desc"
+            -- see :help oil-columns to see which columns are sortable
+            { 'type', 'asc' },
+            { 'name', 'asc' },
+          },
+          -- Customize the highlight group for the file name
+          highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+            return nil
+          end,
+        },
+        -- Extra arguments to pass to SCP when moving/copying files over SSH
+        extra_scp_args = {},
+        -- EXPERIMENTAL support for performing file operations with git
+        git = {
+          -- Return true to automatically git add/mv/rm files
+          add = function(path)
+            return false
+          end,
+          mv = function(src_path, dest_path)
+            return false
+          end,
+          rm = function(path)
+            return false
+          end,
+        },
+        -- Configuration for the floating window in oil.open_float
+        float = {
+          -- Padding around the floating window
+          padding = 2,
+          -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+          max_width = 0,
+          max_height = 0,
+          border = nil,
+          win_options = {
+            winblend = 0,
+          },
+          -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+          get_win_title = nil,
+          -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+          preview_split = 'auto',
+          -- This is the config that will be passed to nvim_open_win.
+          -- Change values here to customize the layout
+          override = function(conf)
+            return conf
+          end,
+        },
+        -- Configuration for the file preview window
+        preview_win = {
+          -- Whether the preview window is automatically updated when the cursor is moved
+          update_on_cursor_moved = true,
+          -- How to open the preview window "load"|"scratch"|"fast_scratch"
+          preview_method = 'fast_scratch',
+          -- A function that returns true to disable preview on a file e.g. to avoid lag
+          disable_preview = function(filename)
+            return false
+          end,
+          -- Window-local options to use for preview window buffers
+          win_options = {},
+        },
+        -- Configuration for the floating action confirmation window
+        confirmation = {
+          -- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+          -- min_width and max_width can be a single value or a list of mixed integer/float types.
+          -- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
+          max_width = 0.9,
+          -- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
+          min_width = { 40, 0.4 },
+          -- optionally define an integer/float for the exact width of the preview window
+          width = nil,
+          -- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+          -- min_height and max_height can be a single value or a list of mixed integer/float types.
+          -- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
+          max_height = 0.9,
+          -- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
+          min_height = { 5, 0.1 },
+          -- optionally define an integer/float for the exact height of the preview window
+          height = nil,
+          border = nil,
+          win_options = {
+            winblend = 0,
+          },
+        },
+        -- Configuration for the floating progress window
+        progress = {
+          max_width = 0.9,
+          min_width = { 40, 0.4 },
+          width = nil,
+          max_height = { 10, 0.9 },
+          min_height = { 5, 0.1 },
+          height = nil,
+          border = nil,
+          minimized_border = 'none',
+          win_options = {
+            winblend = 0,
+          },
+        },
+        -- Configuration for the floating SSH window
+        ssh = {
+          border = nil,
+        },
+        -- Configuration for the floating keymaps help window
+        keymaps_help = {
+          border = nil,
+        },
+      }
+    end,
+  },
+
+  -- nvim v0.8.0
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>g', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+
+  {
+    'kawre/leetcode.nvim',
+    --build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+      -- include a picker of your choice, see picker section for more details
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+    },
+    opts = {
+      ---@type string
+      arg = 'leetcode.nvim',
+
+      ---@type lc.lang
+      lang = 'cpp',
+
+      cn = { -- leetcode.cn
+        enabled = false, ---@type boolean
+        translator = true, ---@type boolean
+        translate_problems = true, ---@type boolean
+      },
+
+      ---@type lc.storage
+      storage = {
+        home = vim.fn.stdpath 'data' .. '/leetcode',
+        cache = vim.fn.stdpath 'cache' .. '/leetcode',
+      },
+
+      ---@type table<string, boolean>
+      plugins = {
+        non_standalone = false,
+      },
+
+      ---@type boolean
+      logging = true,
+
+      injector = {}, ---@type table<lc.lang, lc.inject>
+
+      cache = {
+        update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
+      },
+
+      editor = {
+        reset_previous_code = true, ---@type boolean
+        fold_imports = true, ---@type boolean
+      },
+
+      console = {
+        open_on_runcode = true, ---@type boolean
+
+        dir = 'row', ---@type lc.direction
+
+        size = { ---@type lc.size
+          width = '90%',
+          height = '75%',
+        },
+
+        result = {
+          size = '60%', ---@type lc.size
+        },
+
+        testcase = {
+          virt_text = true, ---@type boolean
+
+          size = '40%', ---@type lc.size
+        },
+      },
+
+      description = {
+        position = 'left', ---@type lc.position
+
+        width = '40%', ---@type lc.size
+
+        show_stats = true, ---@type boolean
+      },
+
+      ---@type lc.picker
+      picker = { provider = nil },
+
+      hooks = {
+        ---@type fun()[]
+        ['enter'] = {},
+
+        ---@type fun(question: lc.ui.Question)[]
+        ['question_enter'] = {},
+
+        ---@type fun()[]
+        ['leave'] = {},
+      },
+
+      keys = {
+        toggle = { 'q' }, ---@type string|string[]
+        confirm = { '<CR>' }, ---@type string|string[]
+
+        reset_testcases = 'r', ---@type string
+        use_testcase = 'U', ---@type string
+        focus_testcases = 'H', ---@type string
+        focus_result = 'L', ---@type string
+      },
+
+      ---@type lc.highlights
+      theme = {},
+
+      ---@type boolean
+      image_support = false,
+    },
+  },
+
+  {
+    'mbbill/undotree',
+    config = function()
+      vim.keymap.set('n', '<leader>i', vim.cmd.UndotreeToggle)
+    end,
+  },
+
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
@@ -772,7 +1125,7 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
@@ -967,6 +1320,9 @@ require('lazy').setup({
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
+  -- TODO:
+  --
+  { 'nvim-mini/mini.nvim', version = false },
 
   -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
   --
